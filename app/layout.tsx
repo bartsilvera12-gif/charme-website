41eeb783bacdd1761394edfb51729c69ba7ddb91
bartsilvera12-gif <bartsilvera12@gmail.com>
2026-08-10 16:54:18 -0300

    Agregado del favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,14 @@ import "./globals.css";
 
 export const metadata: Metadata = {
   title: "Academia CHARME",
-  description: "Formación profesional en belleza, colorimetría, barbería y maquillaje.",
+  description:
+    "Formación profesional en belleza, colorimetría, barbería y maquillaje.",
+
+  icons: {
+    icon: "/images/favicon.ico",
+    shortcut: "/images/favicon.ico",
+    apple: "/images/favicon.ico",
+  },
 };
 
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
